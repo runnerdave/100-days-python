5e2170280d75816e3abd0933719c37fde8921807
David Noguera <david.noguera@techmahindra.com>
2023-08-16 17:11:51 +1000

    day4

--- day4.py
+++ day4.py
@@ -20,6 +20,8 @@
 # And also how you will give feedback to the player.
 # You can find the "official" rules of the game on the World Rock Paper Scissors Association website.
 
+import random
+
 rock = '''
     _______
 ---'   ____)
@@ -48,3 +50,31 @@ scissors = '''
 '''
 
 #Write your code below this line 👇
+
+choices = [rock, paper, scissors]
+
+rules = {
+    (0, 1): "lose",
+    (0, 2): "win",
+    (1, 0): "win",
+    (1, 2): "lose",
+    (2, 0): "lose",
+    (2, 1): "lose",
+}
+
+def play_paper_scissors_rock():
+    while 1==1:
+        user_choice = int(input("What do you choose? Type 0 for Rock, 1 for Paper or 2 for Scissors.\n"))
+        if user_choice < 0 or user_choice > 2:
+            print('Wrong choice')
+            return
+        print(f'You chose:\n{choices[user_choice]}')
+        computer_choice = random.randint(0, 2)
+        print(f'Computer chose:\n{choices[computer_choice]}')
+        if user_choice == computer_choice:
+            print('Result: draw')
+            continue
+        print(f'Result:{rules[user_choice, computer_choice]}')
+
+if __name__ == '__main__':
+    play_paper_scissors_rock()
