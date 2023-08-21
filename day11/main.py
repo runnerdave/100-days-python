94b90670ad1bea317fac5fa0d51dcf0a136becff
David Noguera <david.noguera@techmahindra.com>
2023-08-21 10:30:44 +1000

    day11 starter code

---
+++ day11/main.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+
+# teacher solution: https://replit.com/@appbrewery/blackjack-final?v=1
+
+# instructions
+
+# Do you want to play a game of Blackjack? Type 'y' or 'n': 
+#    Your cards: [5, 10], current score: 15
+#    Computer's first card: 9
+# Type 'y' to get another card, type 'n' to pass: 
+
+from art import logo
+
+cards = {}
+
+
+def play():
+    return
+
+
+if __name__ == '__main__':
+    print(logo)
+    play()
