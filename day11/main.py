a682d888894462abc610fd6df73ca5a3425f661b
David Noguera <david.noguera@techmahindra.com>
2023-08-22 16:46:25 +1000

    Python solution

--- day11/main.py
+++ day11/main.py
@@ -10,14 +10,87 @@
 # Type 'y' to get another card, type 'n' to pass: 
 
 from art import logo
+from cards import Deck
 
-cards = {}
+import os
+   
+os.environ['PYTHONDONTWRITEBYTECODE'] = '1'
 
+deck = Deck()
+
+messages = {
+    "you_win": "You win 😃",
+    "you_lose": "You lose 😤",
+    "you_over": "You went over. You lose 😭",
+    "comp_over": "Opponent went over. You win 😁",
+    "draw": "Draw 🙃",
+}
+
+def draw_round():
+    user_cards = []
+    for _ in range(2):
+        user_cards.append(deck.draw_card())
+    computer_cards = [deck.draw_card()]
+    return user_cards, computer_cards
+
+def sum_hand(cards):
+    hand = 0
+    for c in cards:
+        if c.rank.isnumeric():
+            hand += int(c.rank)
+        elif c.rank == "Ace":
+            hand += 1
+        else:
+            hand += 10
+    return hand
+
+def show_hands(user_cards, computer_cards):
+    print(f"  Your cards: {user_cards}, current score: {sum_hand(user_cards)}")
+    print(f"  Computer's first card {computer_cards}: {sum_hand(computer_cards)}")
+
+def determine_winner(user_cards, computer_cards):
+    user = sum_hand(user_cards)
+    comp = sum_hand(computer_cards)
+    result = user - comp
+    if result > 0:
+        print(messages["you_win"])
+    elif result < 0:
+        print(messages["you_lose"])
+    else:
+        print(messages["draw"])
+
+def computer_draws(computer_cards):
+    while sum_hand(computer_cards) <= 17:
+        computer_cards.append(deck.draw_card())
+    return computer_cards
 
 def play():
-    return
-
+    while True:
+        finish_game = True
+        keep_playing = input("Do you want to play a game of Blackjack? Type 'y' or 'n': ")
+        if keep_playing != 'y':
+            break
+        user_cards, computer_cards = draw_round()
+        show_hands(user_cards, computer_cards)
+        draw_more = 'y'
+        while draw_more == 'y':
+            draw_more = input("Type 'y' to get another card, type 'n' to pass: ")
+            if draw_more != 'y':
+                computer_draws(computer_cards)
+                show_hands(user_cards, computer_cards)
+                if sum_hand(computer_cards) > 21:
+                    print(messages["comp_over"])  
+                    finish_game = False                  
+                break
+            user_cards.append(deck.draw_card())
+            show_hands(user_cards, computer_cards)
+            if sum_hand(user_cards) > 21:
+                print(messages["you_over"])
+                finish_game = False 
+                break
+        if finish_game:
+            determine_winner(user_cards, computer_cards)        
 
 if __name__ == '__main__':
-    print(logo)
+    print(logo)    
     play()
