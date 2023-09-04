0fe59430a61672dbee37b954c3959992eb17ae4d
David Noguera <david.noguera@techmahindra.com>
2023-09-04 11:41:37 +1000

    initial pong project commit

---
+++ day22/main.py
@@ -0,0 +1,12 @@
+from turtle import Snake, Turtle
+
+# PONG!
+# Classes:
+# - Paddle
+#   - Length, move_up, move_down, 
+# - Ball
+#   - launch, bounce, move, touch wall
+# - Scoreboard
+#   - score_counter, call game_over, score
+# - Net
+#   - Length, Segment_length, Segment_space, draw_itself
