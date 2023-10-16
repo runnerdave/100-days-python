81c9c67f9042e87a55d4e23ff59082cbed66a1ca
David Noguera <david.noguera@techmahindra.com>
2023-10-16 18:27:19 +1100

    ui for pomodoro app

--- day28/main.py
+++ day28/main.py
@@ -17,6 +17,12 @@ LONG_BREAK_MIN = 20
 
 # ---------------------------- TIMER RESET ------------------------------- # 
 
+def start_timer():
+    pass
+
+def stop_timer():
+    pass
+
 # ---------------------------- TIMER MECHANISM ------------------------------- # 
 
 # ---------------------------- COUNTDOWN MECHANISM ------------------------------- # 
@@ -29,7 +35,21 @@ window.config(padx=100, pady=50, bg=YELLOW)
 canvas = Canvas(width=200, height=224, bg=YELLOW, highlightthickness=0)
 tomato_img = PhotoImage(file=file_path)
 canvas.create_image(100, 112, image=tomato_img)
+
+timer_label = Label(text="Timer", fg=GREEN, bg=YELLOW, font=(FONT_NAME, 40, FONT_BOLD))
+timer_label.grid(column=1, row=0)
+
 canvas.create_text(100, 112, fill="white", text="00:00", font=(FONT_NAME, 35, FONT_BOLD))
-canvas.pack()
+canvas.grid(column=1, row=1)
+
+start_button = Button(text="Start", bg=YELLOW, command=start_timer)
+start_button.grid(column=0, row=2)
+
+stop_button = Button(text="Stop", bg=YELLOW, command=stop_timer)
+stop_button.grid(column=2, row=2)
+
+blocks_label = Label(text="✓", fg=GREEN, bg=YELLOW, font=(FONT_NAME, 24, FONT_BOLD))
+blocks_label.grid(column=1, row=3)
+
 
 window.mainloop()
