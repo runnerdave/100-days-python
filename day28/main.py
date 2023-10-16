614bb02f47d5b3e1fe667653abca40d55bc0cc2e
David Noguera <david.noguera@techmahindra.com>
2023-10-16 18:35:55 +1100

    ui challenge for pomodoro finish

--- day28/main.py
+++ day28/main.py
@@ -36,16 +36,16 @@ canvas = Canvas(width=200, height=224, bg=YELLOW, highlightthickness=0)
 tomato_img = PhotoImage(file=file_path)
 canvas.create_image(100, 112, image=tomato_img)
 
-timer_label = Label(text="Timer", fg=GREEN, bg=YELLOW, font=(FONT_NAME, 40, FONT_BOLD))
+timer_label = Label(text="Timer", fg=GREEN, bg=YELLOW, font=(FONT_NAME, 40))
 timer_label.grid(column=1, row=0)
 
 canvas.create_text(100, 112, fill="white", text="00:00", font=(FONT_NAME, 35, FONT_BOLD))
 canvas.grid(column=1, row=1)
 
-start_button = Button(text="Start", bg=YELLOW, command=start_timer)
+start_button = Button(text="Start", bg=YELLOW, command=start_timer, highlightbackground=YELLOW, highlightcolor=YELLOW)
 start_button.grid(column=0, row=2)
 
-stop_button = Button(text="Stop", bg=YELLOW, command=stop_timer)
+stop_button = Button(text="Stop", bg=YELLOW, command=stop_timer, highlightbackground=YELLOW, highlightcolor=YELLOW)
 stop_button.grid(column=2, row=2)
 
 blocks_label = Label(text="✓", fg=GREEN, bg=YELLOW, font=(FONT_NAME, 24, FONT_BOLD))
