18505d52d3cef7753b3beebb0c827182a2c2daf3
David Noguera <david.noguera@techmahindra.com>
2023-10-26 19:31:41 +1100

    finish flash cards project

--- day31/main.py
+++ day31/main.py
@@ -1,5 +1,4 @@
 from tkinter import *
-from tkinter import messagebox
 from PIL import ImageTk, Image
 import pandas as pd
 import os
@@ -8,34 +7,23 @@ current_directory = os.path.dirname(os.path.abspath(__file__))
 images_directory = "images"
 data_directory = "data"
 timer = None
+current_word = ""
+words_to_learn_path = os.path.join(current_directory, data_directory, 'words_to_learn.csv')
 
 BACKGROUND_COLOR = "#B1DDC6"
-
-def prep_image(path, size):
-    og = Image.open(path)
-    resized = og.resize(size)
-    img = ImageTk.PhotoImage(resized)
-    return img
-
-def change_word():
-    global timer
-    window.after_cancel(timer)
-    new_word = random.choice(german_dict)
-    canvas.itemconfig(card_image, image=card_front_img)
-    canvas.itemconfig(title_text, fill="black", text=list(new_word.keys())[False])
-    canvas.itemconfig(word_text, fill="black", text=new_word['german'])
-    timer = window.after(3000, show_solution, new_word)
-
-def show_solution(new_word):
-    canvas.itemconfig(card_image, image=card_back_img)
-    canvas.itemconfig(title_text, text=list(new_word.keys())[True], fill="white")
-    canvas.itemconfig(word_text, text=new_word['english'], fill="white")
-    
+ 
 french_words = pd.read_csv(os.path.join(current_directory, data_directory, 'french_words.csv'))
 french_dict = french_words.to_dict('records')
 
 german_words = pd.read_csv(os.path.join(current_directory, data_directory, 'german-top-words.csv'))
 german_dict = german_words.iloc[:, 1:].to_dict('records')
+
+words_to_learn = None
+if os.path.exists(words_to_learn_path):
+    words_to_learn = pd.read_csv(words_to_learn_path).to_dict('records')
+else:
+    words_to_learn = german_dict #Change to french_dict for french
+
 
 card_back = "card_back.png"
 card_back_path = os.path.join(current_directory, images_directory, card_back)
@@ -49,6 +37,35 @@ right_path = os.path.join(current_directory, images_directory, right)
 wrong = "wrong.png"
 wrong_path = os.path.join(current_directory, images_directory, wrong)
 
+def prep_image(path, size):
+    og = Image.open(path)
+    resized = og.resize(size)
+    img = ImageTk.PhotoImage(resized)
+    return img
+
+def change_word():
+    global timer, current_word
+    window.after_cancel(timer)
+    current_word = random.choice(words_to_learn)
+    canvas.itemconfig(card_image, image=card_front_img)
+    canvas.itemconfig(title_text, fill="black", text=list(current_word.keys())[False])
+    canvas.itemconfig(word_text, fill="black", text=list(current_word.values())[False])
+    timer = window.after(3000, show_solution)
+
+def show_solution():
+    global current_word
+    canvas.itemconfig(card_image, image=card_back_img)
+    canvas.itemconfig(title_text, text=list(current_word.keys())[True], fill="white")
+    canvas.itemconfig(word_text, text=list(current_word.values())[True], fill="white")
+
+def learn_word():
+    global current_word
+    target = list(current_word.values())[True]
+    new_list = [item for item in german_dict if target not in item.values()]
+    df = pd.DataFrame(new_list)
+    df.to_csv(words_to_learn_path, index=False)
+    change_word()
+   
 window = Tk()
 window.title("Flash cards")
 window.config(padx=50, pady=50, background=BACKGROUND_COLOR)
@@ -64,7 +81,7 @@ word_text = canvas.create_text(300, 220, fill="black", text="word", font=("Arial
 canvas.grid(column=0, row=0, columnspan=2)
 
 right_img = prep_image(right_path, (50,50))
-right_button = Button(image=right_img, highlightbackground=BACKGROUND_COLOR, command=change_word)
+right_button = Button(image=right_img, highlightbackground=BACKGROUND_COLOR, command=learn_word)
 right_button.grid(column=0, row=1)
 
 wrong_img = prep_image(wrong_path, (50,50))
@@ -73,6 +90,5 @@ wrong_button.grid(column=1, row=1)
 
 timer = window.after(3000, show_solution)
 change_word()
-
 
 window.mainloop()
